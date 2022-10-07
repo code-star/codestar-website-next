775e88f2bcedc043314602a17414846f9e7f4874
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-10-07 18:23:43 +0000

    Deploying to gh-pages from @ code-star/codestar-website-next@8ff117d11e41d6b395d9a22187be630cb6b21299 🚀

--- README.md
+++
@@ -1,9 +0,0 @@
-# Codestar blog
-
-ssg with nextjs
-
-## build
-
-- develop: `npm run dev`
-- static build: `npx next build && npx next export`
-- serve static build `serve -p 5000 out/`
--- lib/meetup/meetup.types.ts
+++
@@ -1,9 +0,0 @@
-export interface IMeetupEvent {
-  name: string;
-  time: string;
-  link: string;
-  description: string;
-  featured_photo: string;
-}
-
-export type MeetupResponse = Array<IMeetupEvent>;
