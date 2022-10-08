a95535c16374f1661825eb8c93bc0c48098e4aa8
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-10-08 20:54:19 +0200

    fix: fix youtube item links

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ ssg with nextjs
 
 ## build
 
+- under github.com > settings > environment > environment secrets > add secret:
+    - YOUTUBE_API_KEY
+    - YOUTUBE_PLAYLIST_ID
 - develop: `npm run dev` -> http://localhost:3000/codestar-website-next
 - static build: `npx next build && npx next export`
 - serve static build `serve -p 5000 out/`
--- components/PlaylistItemCard/PlaylistItemCard.tsx
+++ components/PlaylistItemCard/PlaylistItemCard.tsx
@@ -1,28 +1,24 @@
-import Image from "next/image";
 import { FC } from "react";
 import { formatDate } from "../../lib/formatDate";
 import { IPlaylistItem } from "../../lib/youtube-playlist/youtube-playlist.types";
 import styles from "./PlaylistItemCard.module.scss";
 
 const PlaylistItemCard: FC<{ item: IPlaylistItem }> = ({ item }) => {
-  const { thumbnails, title, publishedAt } = item;
+  const { thumbnails, title, publishedAt, id } = item;
+
   return (
-    <section className={styles["playlistitem-card"]}>
-      <div
-        className="img"
-        style={{
-          backgroundImage: `url(${thumbnails.default.url})`,
-        }}
-      ></div>
-      {/* <Image
-        alt={`Frame from the video "${title}"`}
-        width="250px"
-        height="120px"
-        src={thumbnails.default.url}
-      /> */}
-      <h3>{title}</h3>
-      <p>{formatDate(publishedAt)}</p>
-    </section>
+    <a href={`https://youtube.com/watch?v=${id}`}>
+      <section className={styles["playlistitem-card"]}>
+        <div
+          className="img"
+          style={{
+            backgroundImage: `url(${thumbnails.default.url})`,
+          }}
+        ></div>
+        <h3>{title}</h3>
+        <p>{formatDate(publishedAt)}</p>
+      </section>
+    </a>
   );
 };
 
