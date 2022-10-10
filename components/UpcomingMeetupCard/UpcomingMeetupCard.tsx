ea135f3666a0de43331da76da45b2daf58cf59f3
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-10-10 19:58:31 +0200

    fix: restyle upcoming meetup

--- components/UpcomingMeetupCard/UpcomingMeetupCard.tsx
+++ components/UpcomingMeetupCard/UpcomingMeetupCard.tsx
@@ -1,18 +1,27 @@
 import { FC } from "react";
 import { IMeetupEvent } from "../../lib/meetup/meetup.types";
 import Image from "next/image";
-import { formatDate } from "../../lib/formatDate";
+
 import styles from "./UpcomingMeetupCard.module.scss";
 
 interface IUpcomingMeetupCardProps {
   meetup: IMeetupEvent;
 }
 
+const formatDate = (time: string) =>
+  new Date(time).toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+
 const UpcomingMeetupCard: FC<IUpcomingMeetupCardProps> = ({ meetup }) => {
   const { link, featured_photo, name, time } = meetup;
   return (
     <a href={link}>
       <section className={styles["upcoming-meetup-card"]}>
+        <h2>MEETUP</h2>
+        <h3>{name}</h3>
         {featured_photo && (
           <Image
             alt=""
@@ -21,9 +30,9 @@ const UpcomingMeetupCard: FC<IUpcomingMeetupCardProps> = ({ meetup }) => {
             height={200}
           />
         )}
-        <h3>{name}</h3>
-        <p>{formatDate(time)}</p>
-        <a href={link}>SIGN UP!</a>
+        <p>
+          <a href={link}>SIGN UP FOR {formatDate(time).toUpperCase()}</a>
+        </p>
       </section>
     </a>
   );
