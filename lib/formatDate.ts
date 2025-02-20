11eb6a51bc35460f0b1ab64823bedf0f1ba30e22
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-02-20 12:11:32 +0100

    fix: fix static build

--- lib/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (time: string) =>
-  new Date(time).toLocaleDateString("en-GB", {
-    weekday: 'short',
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-  });
